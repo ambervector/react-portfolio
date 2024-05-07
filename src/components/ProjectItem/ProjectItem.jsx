37e98000718d3c2d8e3bf876e20ad37e89448a57
Mohammad Amber Alam <amberalam.3101@gmail.com>
2024-05-07 15:08:23 +0530

    Projects page made responsive

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import Box from "@mui/material/Grid";
+import Grid from "@mui/material/Box";
+
 import classes from "./ProjectItem.module.css";
 
 const ProjectItem = (props) => {
   return (
     <>
-      <div className={classes.card}>
-        <div className={classes.left_side}>
+      <Grid container spacing={2} columns={16} className={classes.card}>
+        <Grid item xs={12} className={classes.left_side}>
           <h3 className={classes.project_title}>{props.title}</h3>
           <Stack spacing={2} direction="row" className={classes.actions}>
             <a
@@ -18,22 +20,20 @@ const ProjectItem = (props) => {
             >
               Demo Link
             </a>
-            <Button variant="contained" color="error">
-              <a
-                className={classes.btn}
-                href={props.github_link}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Source Code
-              </a>
-            </Button>
+            <a
+              className={classes.btn}
+              href={props.github_link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Source Code
+            </a>
           </Stack>
-        </div>
-        <div className={classes.right_side}>
+        </Grid>
+        <Grid item xs={12} className={classes.right_side}>
           <p>{props.description}</p>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
     </>
   );
 };
